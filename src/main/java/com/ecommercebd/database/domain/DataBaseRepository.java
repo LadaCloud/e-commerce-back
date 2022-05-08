44af4c7ef60d613fc755c9b58e16e00e74f62bde
Alex <alex.silva@algaworks.com.br>
2022-05-08 20:36:55 -0300

    fix

--- src/main/java/com/ecommercebd/database/domain/DataBaseRepository.java
+++ src/main/java/com/ecommercebd/database/domain/DataBaseRepository.java
@@ -1,13 +1,11 @@
 package com.ecommercebd.database.domain;
 
-import com.ecommercebd.user.domain.User;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface DataBaseRepository {
 	List<DataBase> findAll();
-	User save(DataBase entity);
+	DataBase save(DataBase entity);
 	Optional<DataBase> findById(Long id);
 	void delete(DataBase entity);
 }
