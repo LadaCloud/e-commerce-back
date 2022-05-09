d4ab80cfff695d11024eeb61853f60971103cec6
Alex <alex.silva@algaworks.com.br>
2022-05-08 21:24:12 -0300

    database service

--- src/main/java/com/ecommercebd/database/infra/MySQLDataBaseIntegrationManager.java
+++ src/main/java/com/ecommercebd/database/infra/MySQLDataBaseIntegrationManager.java
@@ -4,7 +4,12 @@ import com.ecommercebd.database.domain.DataBase;
 import com.ecommercebd.database.domain.DataBaseIntegrationManager;
 
 public class MySQLDataBaseIntegrationManager implements DataBaseIntegrationManager {
-	
+
+	@Override
+	public void ping(DataBase dataBase) {
+		
+	}
+
 	@Override
 	public void create(DataBase dataBase) {
 		
